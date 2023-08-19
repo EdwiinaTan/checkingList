218637849c255c5d73ff0524650cb0437b4e3d27
EdwiinaTan <edwina.tan77@gmail.com>
2023-08-19 23:48:00 +0200

    pool query

--- server/server.js
+++ server/server.js
@@ -3,15 +3,17 @@ const express = require("express")
 const app = express()
 const pool = require("./db")
 
-// app.get("/", (req, res) => {
-//   res.send("Server port")
-// })
-
-// get list
+app.get("/", (req, res) => {
+  res.send(`Server port: ${process.env.PORT}`)
+})
 
 app.get("/list", async (req, res) => {
+  const userEmail = "test@test.com"
+
   try {
-    const list = await pool.query("SELECT * FROM list")
+    const list = await pool.query("SELECT * FROM list where user_email = $1", [
+      userEmail,
+    ])
     res.json(list.rows)
   } catch (err) {
     console.error(err)
